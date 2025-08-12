2ccdce2d3754d5d72de3cefe8f936b5c9ea41877
shabani005 <hasan.shabani2005@gmail.com>
2025-08-12 13:53:09 +0300

    generalized the code much more. moving closer to a STB style library now. (which is good)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+// #include <math.h>
 // make an allocator for this later
 typedef struct{
   float* x;
@@ -31,9 +31,7 @@ float normalizef(float array[], size_t size){
 #define normalize(arr) normalizef((arr), sizeof(arr) / sizeof((arr)[0]))
 // there are probably better ways to do this but it is what it is 
 
-#define N_SAMPLES 5 // easy to make auto but not bothered
 #define LEARNING_R 0.1f
-// #define EPOCHS 100000000 // allow passing epochs by argv later. kinda cool 
 #define OUTPUT 10
 
 float random_float(){
@@ -58,7 +56,7 @@ int main(int argc, char** argv){
   sample timestwo = {
     .x = x_arr,
     .y = y_arr,
-    .size = 5
+    .size = sizeof(x_arr)/sizeof(x_arr[0])
   };
 
   srand(time(0));
@@ -67,16 +65,16 @@ int main(int argc, char** argv){
     float total_loss = 0.0f;
     float grad = 0.0f;
 
-    for (int i=0; i < N_SAMPLES; ++i){
+    for (int i=0; i < timestwo.size; ++i){
       float x = timestwo.x[i]; // make a variadic macro to generalize the way this works
       float y = timestwo.y[i];
       float y_hat = x * weight;
-      float error = y_hat - y; //maybe do abs later
+      float error = y_hat - y; 
       total_loss += error*error;
       grad += 2*error*x;
     }
-    total_loss /= N_SAMPLES;
-    grad /= N_SAMPLES;
+    total_loss /= timestwo.size;
+    grad /= timestwo.size;
     
     weight -= LEARNING_R*grad;
     if (epoch % (EPOCHS/OUTPUT) == 0){
@@ -86,7 +84,7 @@ int main(int argc, char** argv){
   }
     printf("\nTrained weight: %f\n", weight);
   
-  for (int i = 0; i < N_SAMPLES; ++i) {
+  for (int i = 0; i < timestwo.size; ++i) {
       float x = timestwo.x[i];
       float y = timestwo.y[i];
       float y_pred = weight * x;
